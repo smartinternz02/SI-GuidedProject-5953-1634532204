103d2492966962c8f70979f6dc80bd98846bd741
Pooja R <87558942+chocolateislove@users.noreply.github.com>
2021-11-25 20:10:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,11 +62,9 @@ def predict():
     
     return render_template('home.html',prediction_text="{}".format(emotion),data=list)
 
-port= os.getenv('VCAP_APP_PORT', '8080')
 
 if __name__ == "__main__":
-    app.secret_key= os.urandom(12)
-    app.run(debug= True, host='0.0.0.0', port=port)
+    app.run(debug= False)
     
 
     
